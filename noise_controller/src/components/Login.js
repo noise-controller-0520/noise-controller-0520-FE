99deb5acb5c1305d03092a0cbd30bcd9f119ca27
James Pak <jamesjiminpak@gmail.com>
2019-05-18 16:36:55 -0700

    login page scaffolding

---
+++ noise_controller/src/components/Login.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { connect } from "react-redux";
+import { login } from "../actions";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+
+    this.props.login(this.state.credentials).then(() => {
+      this.props.history.push("/protected");
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            onChange={this.handleChange}
+            value={this.state.credentials.username}
+          />
+
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.credentials.password}
+          />
+
+          <button onClick={this.login}> Log in </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoggingIn: state.isLoggingIn
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
