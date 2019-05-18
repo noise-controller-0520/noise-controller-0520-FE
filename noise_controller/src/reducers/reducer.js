1fc9fee20a51cd0cc6c576b54fc046cc3e789d3d
James Pak <jamesjiminpak@gmail.com>
2019-05-18 15:51:22 -0700

    login reducer set up

---
+++ noise_controller/src/reducers/reducer.js
@@ -0,0 +1,34 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE
+} from '../actions';
+
+const initialState = {
+    isLoggingIn: false,
+    error: ''
+}
+
+export const reducer = ( state = initialState, action ) => {
+    switch (action.type) {
+        case LOGIN_START:
+        return {
+            ...state,
+            isLoggingIn: true,
+            error: ''
+        }
+
+        case LOGIN_SUCCESS:
+        return {
+            ...state,
+            isLoggingIn: false,
+            error: ''
+        }
+
+        case LOGIN_FAILURE:
+        return {
+            ...state,
+            isLoggingIn: false
+        }
+    }
+}
